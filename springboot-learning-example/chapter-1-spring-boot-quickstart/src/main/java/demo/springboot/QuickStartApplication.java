06533968b66a155d9d8aa5b18f264426187d41f8
lpl <785028079@qq.com>
2018-10-11 16:48:31 +0800

    idea

--- springboot-learning-example/chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/QuickStartApplication.java
+++ springboot-learning-example/chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/QuickStartApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Spring Boot 应用启动类
+ * Spring Boot 应用启动类 cehshi
  *
  * Created by bysocket on 26/09/2017.
  */
